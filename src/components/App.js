7ec6f47c9176e3c17ecccec10ca33ba8c3af4c50
Koh <xxx>
2022-04-15 21:54:47 -0700

    Basic form validation (all fields must be full)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [stories, setStories] = useState([]);
+
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.title) {
+      errors.title = 'Required title!';
+    }
+    if (!values.description) {
+      errors.description = 'Required description!';
+    }
+    if (!values.author) {
+      errors.author = 'Required author!';
+    }
+    if (!values.body) {
+      errors.body = 'Required body!';
+    }
+    return errors;
+  };
 
   return (
     <div className="App">
@@ -28,19 +46,24 @@ function App() {
             }
           })
         }}
+        validate={validate}
       >
         <Form>
           <label htmlFor="title">Title: </label>
-          <Field id="title" name="title"/><br />
+          <Field id="title" name="title"/>
+          <ErrorMessage name="title" /><br />
 
           <label htmlFor="description">Description: </label>
-          <Field id="description" name="description"/> <br />
+          <Field id="description" name="description"/> 
+          <ErrorMessage name="description" /><br />
 
           <label htmlFor="author">Author: </label>
-          <Field id="author" name="author"/><br />
+          <Field id="author" name="author"/>
+          <ErrorMessage name="author" /><br />
 
           <label htmlFor="body">Body: </label>
-          <Field id="body" name="body" as="textarea"/><br />
+          <Field id="body" name="body" as="textarea"/>
+          <ErrorMessage name="body" /><br />
 
           <button type="submit">Submit</button>
         </Form>
