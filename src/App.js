70b45960db1bf470526fbaa21f84485e42dcd1e8
Koh <xxx>
2022-06-05 02:49:17 -0700

    some more work on scaling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import StoryList from './components/StoryList';
 import StoryForm from './components/StoryForm';
-import './css/app.css';
+import './css/components/app.css';
 
 function App() {
   return (
--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -5,27 +5,21 @@ function StoryCard(props) {
     <div className='story-card'>
       <div className='content-container'>
         <div className='info-container'>
-          <div className='header-text'>TITLE</div>
-          <div>{props.title}</div>
-          <div className='header-text'>AUTHOR</div>
-          <div>{props.author}</div>
-        </div>
+          <div className='info-container__item'>
+            <div className='header-text'>TITLE</div>
+            <div className='emoji-text'>{props.title}</div>
+          </div>
+          <div className='info-container__item'>
+            <div className='header-text'>AUTHOR</div>
+            <div className='emoji-text'>{props.author}</div>
+          </div>
 
-        <div className='divider-container'>
-          <span style={{"backgroundColor": "white", "writingMode": "vertical-lr", "transform": "rotate(180deg)", "paddingTop": "10px", "paddingBottom": "10px"}}>Emotica</span>
-        </div>
-
+      </div>
+        <div className='divider-container'> <span className='divider-container__text'>emotica</span> </div>
         <div className='description-container'>
-          <div style={{"paddingBottom": "10px"}}>
-            Summary<br/>
-            {props.description}
-          </div>
-          <div style={{"borderTop":"3px dotted black", "paddingTop": "10px"}}>
-          Story<br />
-          {props.body}
-          </div>
+          <div>Summary<br/> {props.description}</div>
+          <div> Story<br /> {props.body}</div>
         </div>
-
       </div>
 
       <div className='deco-container'></div>
